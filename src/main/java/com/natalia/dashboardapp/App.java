119be755b78639c2b752148dba698dcbd9fc708b
Natalia Nikolaeva <n.nikolaeva@gmail.com>
2018-11-08 11:17:57 -0800

    Empty maven project with .gitignore

---
+++ src/main/java/com/natalia/dashboardapp/App.java
@@ -0,0 +1,13 @@
+package com.natalia.dashboardapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
