58abf40086883bc960b341ba4aa6f542a0232b56
Natalia Nikolaeva <n.nikolaeva@gmail.com>
2018-11-08 17:04:02 -0800

    Implement login with google servlet

--- src/main/java/me/nikolaeva/dashboardapp/api/services/LoginServlet.java
+++ src/main/java/me/nikolaeva/dashboardapp/api/services/LoginServlet.java
@@ -1,26 +1,40 @@
 package me.nikolaeva.dashboardapp.api.services;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.inject.Named;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import me.nikolaeva.dashboardapp.proto.User;
-
+import me.nikolaeva.dashboardapp.proto.AppConfig;
 
 @Singleton
 public class LoginServlet extends HttpServlet {
 
+  private final AppConfig appConfig;
+
+  @Inject
+  public LoginServlet(@Named("appConfig") AppConfig appConfig) {
+    this.appConfig = appConfig;
+  }
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    User user = User.getDefaultInstance();
-
-    resp.setContentType("text/html; charset=utf-8");
-    resp.setStatus(HttpServletResponse.SC_OK);
-    PrintWriter out = resp.getWriter();
-    out.println("<h1>response from login servlet</h1>");
+    HttpServletResponse httpResponse = (HttpServletResponse) resp;
+    String url =
+        "https://accounts.google.com/o/oauth2/v2/auth?scope="
+            + appConfig.getGoogleOauthConfig().getScope()
+            + "&redirect_uri="
+            + appConfig.getGoogleOauthConfig().getRedirectUrl()
+            + "&client_id="
+            + appConfig.getGoogleOauthConfig().getClientId()
+            + "&response_type="
+            + appConfig.getGoogleOauthConfig().getResponseType()
+            + "&state="
+            + appConfig.getGoogleOauthConfig().getState();
+    httpResponse.sendRedirect(url);
   }
 }
